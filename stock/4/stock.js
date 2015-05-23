08143ade3439fd70f723e2e3a47e4c0d3ba58809
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-23 17:54:43 +0200

    update long term stock chart properties

--- stock/4/stock.js
+++ stock/4/stock.js
@@ -7,7 +7,6 @@
  */
 function createViewStockCurveFixing(container, width, height) {
 
-
 	var view = new JenScript.View({
 		name : container,
 		width : width,
@@ -18,7 +17,7 @@ function createViewStockCurveFixing(container, width, height) {
 	});
 
 	var startDate = new Date(2009, 09, 01);
-	var endDate = new Date(2014, 11, 01);
+	var endDate = new Date(2015, 11, 01);
 
 	var proj1 = new JenScript.TimeXProjection({
 		cornerRadius : 6,
@@ -34,36 +33,43 @@ function createViewStockCurveFixing(container, width, height) {
 	var outline = new JenScript.DeviceOutlinePlugin({color : 'darkslategrey'});
 	proj1.registerPlugin(outline);
 
-
+	//prepare metrics properties
+	var minor = {
+		tickMarkerSize : 2,
+		tickMarkerColor : JenScript.RosePalette.AEGEANBLUE,
+		tickMarkerStroke : 1
+	};
+	var median = {
+		tickMarkerSize : 4,
+		tickMarkerColor : JenScript.RosePalette.EMERALD,
+		tickMarkerStroke : 1.2,
+		tickTextColor : JenScript.RosePalette.EMERALD,
+		tickTextFontSize : 10
+	};
+	var major = {
+		tickMarkerSize : 8,
+		tickMarkerColor : JenScript.RosePalette.CORALRED,
+		tickMarkerStroke : 3,
+		tickTextColor : JenScript.RosePalette.CORALRED,
+		tickTextFontSize : 12,
+		tickTextOffset : 16
+	};
 	var southMetrics1 = new JenScript.AxisMetricsTiming({
 		axis : JenScript.Axis.AxisSouth,
-		models : [new JenScript.HourModel({}),new JenScript.DayModel({}),new JenScript.MonthModel({})],
-		minor : {
-			tickMarkerSize : 2,
-			tickMarkerColor : JenScript.RosePalette.AEGEANBLUE,
-			tickMarkerStroke : 1
-		},
-		median : {
-			tickMarkerSize : 4,
-			tickMarkerColor : JenScript.RosePalette.EMERALD,
-			tickMarkerStroke : 1.2,
-			tickTextColor : JenScript.RosePalette.EMERALD,
-			tickTextFontSize : 10
-		},
-		major : {
-			tickMarkerSize : 8,
-			tickMarkerColor : JenScript.RosePalette.CORALRED,
-			tickMarkerStroke : 3,
-			tickTextColor : JenScript.RosePalette.CORALRED,
-			tickTextFontSize : 12,
-			tickTextOffset : 16
-		}
+		models : [new JenScript.MonthModel({}), new JenScript.YearModel({})],
+		minor : minor,
+		median : median,
+		major : major,
+		gravity : 'rotate'
 	});
 	proj1.registerPlugin(southMetrics1);
 	
 	
 	var westMetrics = new JenScript.AxisMetricsModeled({
-		axis : JenScript.Axis.AxisWest
+		axis : JenScript.Axis.AxisWest,
+		minor : minor,
+		median : median,
+		major : major
 	});
 	proj1.registerPlugin(westMetrics);
 
@@ -72,8 +78,8 @@ function createViewStockCurveFixing(container, width, height) {
 	proj1.registerPlugin(stockPlugin);
 
 	stockPlugin.addLayer(new JenScript.StockFixingLayer({
-		curveColor :'purple',
-		curveWidth : 2
+		curveColor :'pink',
+		curveWidth : 1
 	}));
 
 	
@@ -82,7 +88,7 @@ function createViewStockCurveFixing(container, width, height) {
 		part   : JenScript.ViewPart.Device,
 		text   : 'SLV Fixing',
 		fontSize : 14,
-		textColor : 'purple',
+		textColor : JenScript.RosePalette.EMERALD,
 		xAlign : 'right',
 		yAlign : 'top',
 	});
