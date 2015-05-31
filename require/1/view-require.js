5b2dd325b4cde83575c52a7e88404c9202b16023
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-31 20:11:09 +0200

    update require articles

--- require/1/view-require.js
+++ require/1/view-require.js
@@ -1,8 +1,6 @@
 
 createView = function(j,container,w,h){
-	
-	
-	
+
 		j.view({
 			name : container,
 			width : w,
@@ -16,7 +14,10 @@ createView = function(j,container,w,h){
 			maxX : 1000,
 			minY : -1000,
 			maxY : 1000
-		}).pie({radius : 120,startAngleDegree : 45}).slice({
+		}).pie({
+			radius : 120,
+			startAngleDegree : 45
+		}).slice({
 			name : "s1",
 			value : 45,
 			themeColor : 'rgba(240, 240, 240, 0.9)',
@@ -26,18 +27,18 @@ createView = function(j,container,w,h){
 			outlineColor : 'rgba(240, 240, 240, 0.9)',
 			cornerRadius : 8,
 			outlineWidth : 2,
-			textColor :JenScript.Color.lighten('rgba(240, 240, 240, 0.9)',20),
+			textColor :j.Color.lighten('rgba(240, 240, 240, 0.9)',20),
 		}).slice({
 			name : "s2",
 			value : 5,
-			themeColor : JenScript.RosePalette.CORALRED
+			themeColor : j.RosePalette.LIME
 		}).label('radial',{
 			text : "Platinium",
 			fillColor:'black',
-			outlineColor : JenScript.Color.lighten(JenScript.RosePalette.CORALRED,20),
+			outlineColor : j.Color.lighten(j.RosePalette.CORALRED,20),
 			cornerRadius : 8,
 			outlineWidth : 2,
-			textColor :JenScript.Color.lighten(JenScript.RosePalette.CORALRED,40),
+			textColor :j.Color.lighten(j.RosePalette.CORALRED,40),
 		}).slice({
 			name : "s3",
 			value : 30,
@@ -48,30 +49,31 @@ createView = function(j,container,w,h){
 			outlineColor : 'rgba(78,148,44,1)',
 			cornerRadius : 8,
 			outlineWidth : 2,
-			textColor :JenScript.Color.lighten('rgba(78,148,44,1)',30),
+			textColor :j.Color.lighten('rgba(78,148,44,1)',30),
 		}).slice({
 			name : "s4",
 			value : 5,
-			themeColor : JenScript.RosePalette.AEGEANBLUE,
-			divergence : 20
-		}).label('radial',{
+			themeColor : j.RosePalette.MANDARIN,
+			divergence : 30,
+		}).label('border',{
 			text : "Chrome",
 			fillColor:'black',
-			outlineColor : JenScript.RosePalette.AEGEANBLUE,
+			outlineColor : j.RosePalette.MANDARIN,
 			cornerRadius : 8,
 			outlineWidth : 2,
-			textColor :JenScript.Color.lighten(JenScript.RosePalette.AEGEANBLUE,30),
+			textColor :j.Color.lighten(j.RosePalette.MANDARIN,30),
+			margin : 100
 		}).slice({
 			name : "s5",
 			value : 5,
-			themeColor : JenScript.RosePalette.INDIGO
+			themeColor : j.RosePalette.INDIGO
 		}).label('radial',{
 			text : "Uranium",
 			fillColor:'black',
-			outlineColor : JenScript.Color.lighten(JenScript.RosePalette.INDIGO,30),
+			outlineColor : j.Color.lighten(j.RosePalette.INDIGO,30),
 			cornerRadius : 8,
 			outlineWidth : 2,
-			textColor :JenScript.Color.lighten(JenScript.RosePalette.INDIGO,50)
+			textColor :j.Color.lighten(j.RosePalette.INDIGO,50)
 		}).effect('linear',{offset : 0,incidence : 300}).effect('reflection');
 
 
