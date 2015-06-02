5137455e546e26287013c2b829fa963721810f36
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-06-02 20:21:15 +0200

    add articles for donut2d, donut3d, pie builder

---
+++ pie/2/pie-builder.js
@@ -0,0 +1,83 @@
+
+createView = function(container,w,h){
+
+		var finalVarIGetByLastMethodChainDude = JenScript.view({
+			name : container,
+			width : w,
+			height : h,
+			holders : 0,
+			north: 20,
+			south :20,
+		}).projection('linear',{
+			name : "proj1",
+			minX : -1000,
+			maxX : 1000,
+			minY : -1000,
+			maxY : 1000
+		}).pie({
+			radius : 120,
+			startAngleDegree : 45
+		}).slice({
+			name : "s1",
+			value : 45,
+			themeColor : 'rgba(240, 240, 240, 0.9)',
+		}).label('radial',{
+			text : "Silver",
+			fillColor:'black',
+			outlineColor : 'rgba(240, 240, 240, 0.9)',
+			cornerRadius : 8,
+			outlineWidth : 2,
+			textColor :JenScript.Color.lighten('rgba(240, 240, 240, 0.9)',20),
+		}).slice({
+			name : "s2",
+			value : 5,
+			themeColor : JenScript.RosePalette.LIME
+		}).label('radial',{
+			text : "Platinium",
+			fillColor:'black',
+			outlineColor : JenScript.Color.lighten(JenScript.RosePalette.CORALRED,20),
+			cornerRadius : 8,
+			outlineWidth : 2,
+			textColor :JenScript.Color.lighten(JenScript.RosePalette.CORALRED,40),
+		}).slice({
+			name : "s3",
+			value : 30,
+			themeColor : 'rgba(78,148,44,1)'
+		}).label('radial',{
+			text : "Rhodium",
+			fillColor:'black',
+			outlineColor : 'rgba(78,148,44,1)',
+			cornerRadius : 8,
+			outlineWidth : 2,
+			textColor :JenScript.Color.lighten('rgba(78,148,44,1)',30),
+		}).slice({
+			name : "s4",
+			value : 5,
+			themeColor : JenScript.RosePalette.MANDARIN,
+			divergence : 30,
+		}).label('border',{
+			text : "Chrome",
+			fillColor:'black',
+			outlineColor : JenScript.RosePalette.MANDARIN,
+			cornerRadius : 8,
+			outlineWidth : 2,
+			textColor :JenScript.Color.lighten(JenScript.RosePalette.MANDARIN,30),
+			margin : 100
+		}).slice({
+			name : "s5",
+			value : 5,
+			themeColor : JenScript.RosePalette.INDIGO
+		}).label('radial',{
+			text : "Uranium",
+			fillColor:'black',
+			outlineColor : JenScript.Color.lighten(JenScript.RosePalette.INDIGO,30),
+			cornerRadius : 8,
+			outlineWidth : 2,
+			textColor :JenScript.Color.lighten(JenScript.RosePalette.INDIGO,50)
+		}).linearFx({offset : 2, fillOpacity : 0.6,incidence : 1200}).reflectFx({}).linearFx({offset :6,fillOpacity : 0.4,incidence : 300}).view();
+		
+		console.log("last var i get is view: "+finalVarIGetByLastMethodChainDude);
+		
+		
+
+}
