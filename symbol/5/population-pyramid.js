bc74f3a4e49f67ef371a754bdb048b27322eb429
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-06-14 10:59:10 +0200

    add population pyramid article

---
+++ symbol/5/population-pyramid.js
@@ -0,0 +1,292 @@
+var data = [];
+var makeEntry = function(year,ellapsedAge,total,men,women){
+	data.push({year : year,ellapsedAge : ellapsedAge,total:total,men:men,women : women});
+}
+makeEntry(2011, 0, 822621, 420230, 402391);
+makeEntry(2010, 1, 825536, 421749, 403787);
+makeEntry(2009, 2, 816582, 417304, 399278);
+makeEntry(2008, 3, 795482, 406612, 388870);
+makeEntry(2007, 4, 794462, 405892, 388570);
+makeEntry(2006, 5, 809828, 414173, 395655);
+makeEntry(2005, 6, 796668, 406870, 389798);
+makeEntry(2004, 7, 793712, 406121, 387591);
+makeEntry(2003, 8, 794984, 405806, 389178);
+makeEntry(2002, 9, 804327, 412429, 391898);
+makeEntry(2001, 10, 818285, 418138, 400147);
+makeEntry(2000, 11, 835515, 428514, 407001);
+makeEntry(1999, 12, 804662, 412252, 392410);
+makeEntry(1998, 13, 798449, 409328, 389121);
+makeEntry(1997, 14, 787265, 404275, 382990);
+makeEntry(1996, 15, 795740, 407808, 387932);
+makeEntry(1995, 16, 791414, 404471, 386943);
+makeEntry(1994, 17, 771655, 393846, 377809);
+makeEntry(1993, 18, 769043, 392453, 376590);
+makeEntry(1992, 19, 809037, 412671, 396366);
+makeEntry(1991, 20, 818155, 414980, 403175);
+makeEntry(1990, 21, 824026, 418549, 405477);
+makeEntry(1989, 22, 822136, 414838, 407298);
+makeEntry(1988, 23, 820659, 414644, 406015);
+makeEntry(1987, 24, 806806, 404859, 401947);
+makeEntry(1986, 25, 808404, 403116, 405288);
+makeEntry(1985, 26, 798896, 396981, 401915);
+makeEntry(1984, 27, 784393, 388019, 396374);
+makeEntry(1983, 28, 771630, 382134, 389496);
+makeEntry(1982, 29, 822730, 406443, 416287);
+makeEntry(1981, 30, 833230, 410364, 422866);
+makeEntry(1980, 31, 845114, 416157, 428957);
+makeEntry(1979, 32, 801192, 395250, 405942);
+makeEntry(1978, 33, 784103, 387876, 396227);
+makeEntry(1977, 34, 796288, 394050, 402238);
+makeEntry(1976, 35, 777116, 384957, 392159);
+makeEntry(1975, 36, 802821, 397647, 405174);
+makeEntry(1974, 37, 850588, 420914, 429674);
+
+makeEntry(1973, 38, 899948, 445510, 454438);
+makeEntry(1972, 39, 926479, 459200, 467279);
+makeEntry(1971, 40, 925679, 458803, 466876);
+makeEntry(1970, 41, 908290, 449601, 458689);
+makeEntry(1969, 42, 895659, 442687, 452972);
+makeEntry(1968, 43, 891852, 440105, 451747);
+makeEntry(1967, 44, 889115, 439398, 449717);
+makeEntry(1966, 45, 910269, 448357, 461912);
+makeEntry(1965, 46, 914040, 450784, 463256);
+makeEntry(1964, 47, 923937, 454069, 469868);
+makeEntry(1963, 48, 912926, 447673, 465253);
+
+makeEntry(1973, 38, 899948, 445510, 454438);
+makeEntry(1972, 39, 926479, 459200, 467279);
+makeEntry(1971, 40, 925679, 458803, 466876);
+makeEntry(1970, 41, 908290, 449601, 458689);
+makeEntry(1969, 42, 895659, 442687, 452972);
+makeEntry(1968, 43, 891852, 440105, 451747);
+makeEntry(1967, 44, 889115, 439398, 449717);
+makeEntry(1966, 45, 910269, 448357, 461912);
+makeEntry(1965, 46, 914040, 450784, 463256);
+makeEntry(1964, 47, 923937, 454069, 469868);
+makeEntry(1963, 48, 912926, 447673, 465253);
+makeEntry(1962, 49, 883679, 432883, 450796);
+makeEntry(1961, 50, 884664, 433513, 451151);
+makeEntry(1960, 51, 879074, 430584, 448490);
+makeEntry(1959, 52, 874272, 427256, 447016);
+makeEntry(1958, 53, 854033, 414679, 439354);
+makeEntry(1957, 54, 851946, 414604, 437342);
+makeEntry(1956, 55, 846074, 409852, 436222);
+makeEntry(1955, 56, 839076, 405479, 433597);
+makeEntry(1954, 57, 837979, 406026, 431953);
+makeEntry(1953, 58, 821197, 396905, 424292);
+makeEntry(1952, 59, 835417, 403582, 431835);
+makeEntry(1951, 60, 815161, 394806, 420355);
+makeEntry(1950, 61, 848175, 410640, 437535);
+makeEntry(1949, 62, 832451, 402234, 430217);
+makeEntry(1948, 63, 829557, 402052, 427505);
+makeEntry(1947, 64, 809323, 391012, 418311);
+makeEntry(1946, 65, 766145, 368862, 397283);
+makeEntry(1945, 66, 579210, 276948, 302262);
+makeEntry(1944, 67, 566870, 269167, 297703);
+makeEntry(1943, 68, 548643, 260639, 288004);
+makeEntry(1942, 69, 510759, 241404, 269355);
+makeEntry(1941, 70, 452659, 211514, 241145);
+makeEntry(1940, 71, 471554, 217922, 253632);
+makeEntry(1939, 72, 492778, 224378, 268400);
+makeEntry(1938, 73, 481422, 215877, 265545);
+makeEntry(1937, 74, 469642, 208991, 260651);
+makeEntry(1936, 75, 470077, 205741, 264336);
+makeEntry(1935, 76, 455631, 195353, 260278);
+makeEntry(1934, 77, 457015, 193672, 263343);
+makeEntry(1933, 78, 434174, 179483, 254691);
+makeEntry(1932, 79, 438426, 178223, 260203);
+makeEntry(1931, 80, 414906, 164605, 250301);
+makeEntry(1930, 81, 405765, 157853, 247912);
+makeEntry(1929, 82, 364739, 138075, 226664);
+makeEntry(1928, 83, 343668, 126198, 217470);
+makeEntry(1927, 84, 316432, 112646, 203786);
+makeEntry(1926, 85, 293242, 101038, 192204);
+makeEntry(1925, 86, 268313, 89419, 178894);
+makeEntry(1924, 87, 236683, 75381, 161302);
+makeEntry(1923, 88, 210595, 64199, 146396);
+makeEntry(1922, 89, 186055, 54897, 131158);
+makeEntry(1921, 90, 163492, 46464, 117028);
+makeEntry(1920, 91, 138392, 37857, 100535);
+makeEntry(1919, 92, 68000, 17105, 50895);
+makeEntry(1918, 93, 48704, 11237, 37467);
+makeEntry(1917, 94, 34609, 7268, 27341);
+makeEntry(1916, 95, 24546, 4802, 19744);
+makeEntry(1915, 96, 22273, 4096, 18177);
+makeEntry(1914, 97, 25438, 4543, 20895);
+makeEntry(1913, 98, 16972, 2476, 14496);
+makeEntry(1912, 99, 10962, 1341, 9621);
+
+data.reverse();
+
+function createView(container,width,height){
+	var view = new JenScript.View({
+		name : container,
+		width : width,
+		height : height,
+		
+		west : 80,
+		south:60
+	});
+
+	var proj = new JenScript.LinearProjection({
+		name : "proj",
+		minX : -700000,
+		maxX : 700000,
+		minY : 0,
+		maxY : 0
+	});
+	view.registerProjection(proj);
+	
+	var outline = new JenScript.DeviceOutlinePlugin({
+		color : 'pink'
+	});
+	proj.registerPlugin(outline);
+	
+	//TOOL
+	var tx1 = new JenScript.TranslatePlugin({
+		mode : 'tx',
+	});
+	proj.registerPlugin(tx1);
+	tx1.registerWidget(new JenScript.TranslateCompassWidget({
+		ringFillColor : 'pink'
+	}));
+	tx1.select();
+	
+	var zoomwheel = new JenScript.ZoomWheelPlugin({
+		mode : 'wx'
+	});
+	proj.registerPlugin(zoomwheel);
+	
+	var metrics = new JenScript.AxisMetricsModeled({
+		axis : JenScript.Axis.AxisSouth,
+		minor : {
+			tickMarkerSize : 2,
+			tickMarkerColor : JenScript.RosePalette.AEGEANBLUE,
+			tickMarkerStroke : 1
+		},
+		median : {
+			tickMarkerSize : 4,
+			tickMarkerColor : JenScript.RosePalette.EMERALD,
+			tickMarkerStroke : 1.2,
+			tickTextColor : JenScript.RosePalette.EMERALD,
+			tickTextFontSize : 10
+		},
+		major : {
+			tickMarkerSize : 8,
+			tickMarkerColor : JenScript.RosePalette.TURQUOISE,
+			tickMarkerStroke : 3,
+			tickTextColor : JenScript.RosePalette.TURQUOISE,
+			tickTextFontSize : 12
+		}
+	});
+	proj.registerPlugin(metrics);
+	
+	
+	var symbolPlugin = new JenScript.SymbolPlugin({
+		nature : 'Horizontal'
+	});
+	proj.registerPlugin(symbolPlugin);
+	
+	
+	
+	
+	
+	//layer
+	var menLayer = new JenScript.SymbolBarLayer();
+	symbolPlugin.addLayer(menLayer);
+	var womenLayer = new JenScript.SymbolBarLayer();
+	symbolPlugin.addLayer(womenLayer);
+	
+	
+	menLayer.addSymbol(JenScript.SymbolFiller.createGlue());
+	womenLayer.addSymbol(JenScript.SymbolFiller.createGlue());
+	
+	for (var i = 0; i < data.length; i++) {
+		var entry = data[i];
+		var men = new JenScript.SymbolBar({
+			base : 0,
+			value: entry.men,
+			thickness : 2,
+			direction : 'ascent',
+			morpheStyle : 'Rectangle',
+			themeColor : '#37c3e0',
+			opacity : 1,
+			barFill : new JenScript.SymbolBarFill0({}),
+			userObject : entry,
+			//barEffect  : new JenScript.SymbolBarEffect0({}), //useless for small bar
+		});
+		
+		var women = new JenScript.SymbolBar({
+			base : 0,
+			value: entry.women,
+			thickness : 2,
+			direction : 'descent',
+			morpheStyle : 'Rectangle',
+			themeColor : '#fc6c85',
+			opacity : 1,
+			barFill : new JenScript.SymbolBarFill0({}),
+			userObject : entry,
+			//barEffect  : new JenScript.SymbolBarEffect0({}),  //useless for small bar
+		});
+		
+		//false flag means does not repaint each time you add symbol
+		menLayer.addSymbol(men,false);
+		womenLayer.addSymbol(women,false);
+	}
+
+	menLayer.addSymbol(JenScript.SymbolFiller.createGlue());
+	womenLayer.addSymbol(JenScript.SymbolFiller.createGlue());
+	
+	//invoke repaint only one time
+	symbolPlugin.repaintPlugin();
+	
+	//label for listener
+	var labelPlugin = new JenScript.TextLabelPlugin();
+	proj.registerPlugin(labelPlugin);
+	
+	var label = new JenScript.TextLabel({
+		fillColor : 'black',
+		outlineColor : JenScript.Color.lighten('rgb(244, 145, 26)',20),
+		cornerRadius : 10,
+		outlineWidth : 2,
+		textColor : JenScript.Color.lighten('rgb(244, 145, 26)',20),
+		fontSize : 16
+	});
+	labelPlugin.addLabel(label);
+
+	var updateText = function(action, event) {
+		var uo = event.symbol.userObject;
+		label.setText(uo.year+' women : '+uo.women+' men : '+uo.men);
+		label.setX(event.device.x);
+		label.setY(event.device.y);
+		labelPlugin.repaintPlugin();
+	};
+	
+	
+	//listener
+	//event is something like, refer to source
+	//event : {symbol : bar, x:x,y:y, device :{x:x,y:y}}
+	
+	
+	menLayer.addSymbolListener('enter',function(event){
+		updateText('enter',event);
+	},'this demo');
+	
+	//listener
+	menLayer.addSymbolListener('exit',function(event){
+		updateText('exit',event);
+	},'this demo');
+	
+	
+	womenLayer.addSymbolListener('enter',function(event){
+		updateText('enter',event);
+	},'this demo');
+	
+	//listener
+	womenLayer.addSymbolListener('exit',function(event){
+		updateText('exit',event);
+	},'this demo');
+	
+	
+
+}
